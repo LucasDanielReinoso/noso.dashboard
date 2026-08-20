a13bdc1064fec758a867d8f7491b6ef5a781771f
Lucas Daniel Reinoso <152199012+LucasDanielReinoso@users.noreply.github.com>
2026-08-20 16:21:26 -0300

    .
    .

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-// firebase-config.js
+// Agrega "export" antes de const
 export const firebaseConfig = {
-  apiKey: "AIzaSyCxGfy4Y4F4C3dPoXUOuXoxZfAfX7ljY2w",
-  authDomain: "noso-pro.firebaseapp.com",
-  projectId: "noso-pro",
-  storageBucket: "noso-pro.firebasestorage.app",
-  messagingSenderId: "801843860180",
-  appId: "1:801843860180:web:bc166745f954eb080e6141"
+  apiKey: "AIzaSyDc4LsqR1Q8qrvugn1DfTfBGvL5esKUCug",
+  authDomain: "noso-database-formosa.firebaseapp.com",
+  projectId: "noso-database-formosa",
+  storageBucket: "noso-database-formosa.firebasestorage.app",
+  messagingSenderId: "841696727752",
+  appId: "1:841696727752:web:59a72c0d0bd061b3f20bfd"
 };
